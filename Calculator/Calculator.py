2f46ed05c41a46201eb09c49fe80d01856604d8d
Wve-program <55114757+Wve-program@users.noreply.github.com>
2019-12-20 19:40:02 -0500

    Task: Definitions

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -12,3 +12,27 @@ class Calculator:
 
     def __init__(self):
         pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+    
+     def substract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+    
+     def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+    
+     def divide(self, a, b):
+        self.result = division(a, b)
+        return self.result
+    
+     def square(self, a, b):
+        self.result = square(a, b)
+        return self.result
+    
+     def squartroot(self, a, b):
+        self.result = squareroot(a, b)
+        return self.result
