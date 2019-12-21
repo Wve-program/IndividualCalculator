7eb674f2704954e2b19149cd213e3a51da5a96f2
Wve-program <55114757+Wve-program@users.noreply.github.com>
2019-12-20 19:36:03 -0500

    Task: Important definitions

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,3 +1,12 @@
+from Calculator.Addition import Addition
+from Calculator.Subtraction import Subtraction
+from Calculator.Multiplication import Multiplication
+from Calculator.Division import Division
+from Calculator.Square import Square
+from Calculator.Squareroot import Squareroot
+
+
+
 class Calculator:
     result = 0
 
